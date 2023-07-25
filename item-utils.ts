d7d90a316d4f4940db494ba9393b5cd05417baac
rcabralse <140479071+rcabralse@users.noreply.github.com>
2023-07-25 08:53:20 -0700

    Delete item-utils.ts

--- item-utils.ts
+++
@@ -1,9 +0,0 @@
-import { html } from 'lit';
-
-export function convertItems(picklistoptions: string) {
-  const outputArray = picklistoptions.split(", ");
-  const htmlOptions = outputArray.map(
-    (item) => html`<option value="${item}">${item}</option>`
-  );
-  return htmlOptions;
-}
